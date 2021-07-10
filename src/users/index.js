2726862b1978dec5dd268aba0459ba7c06cc098e
Kapil Thapliyal <kapilthapliyal0001@gmail.com>
2021-07-10 17:41:23 +0200

    code cleanig

--- src/users/index.js
+++ src/users/index.js
@@ -8,43 +8,7 @@ const userRouter = express.Router();
 
 // User Routes
 
-//  POST the Result;
-userRouter.post("/", async (req, res, next) => {
-  try {
-    const newUser = new UserModel(req.body);
-    const _id = await newUser.save();
-    console.log(newUser);
-    res.status(201).send({_id});
-  } catch (error) {
-    console.log(error);
-    if (error.name === "ValidationError") {
-      next(createError(400, error));
-    } else {
-      next(
-        createError(
-          500,
-          "An error has occured while posting the data of the user! "
-        )
-      );
-    }
-  }
-});
-
-// GET the player names;
-userRouter.get("/", async (req, res, next) => {
-  try {
-    const names = await UserModel.find();
-    console.log(names);
-    res.send(names);
-  } catch (error) {
-    console.log(error);
-    next(
-      createError(500, "An Error has occured while searching for the user!")
-    );
-  }
-});
-
-//Get a specific user
+//Get the old visitor count;
 userRouter.get("/:id", async (req, res, next) => {
   try {
     const names = await UserModel.findById(req.params.id);
@@ -58,6 +22,7 @@ userRouter.get("/:id", async (req, res, next) => {
   }
 });
 
+// Update the visitor number;
 userRouter.put("/:id", async (req, res, next) => {
   try {
     const id = req.params.id;
